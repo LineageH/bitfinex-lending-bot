950a3ec61e0c9d73a22065986f7a8bb17d23dcfc
LineageH <lineage.hui@gmail.com>
2025-04-16 13:07:28 +0800

    minimum offer should be 150

--- server/custom-config.example.js
+++ server/custom-config.example.js
@@ -2,14 +2,14 @@ module.exports = {
   SubmitTime: 5, // The time gap to submit the funding offer (in minutes)
   Strategy: {
     splitEqually: {
-      MIN_TO_LEND: 50,
+      MIN_TO_LEND: 150,
       NUM_ALL_IN: 1100,
       SPLIT_UNIT: 1000,
       RATE_EXPECTED_OVER_AMOUNT: 50000,
     }, // The default strategy is splitEqually, you need to edit the code in server/submit-funding-offer.js to change the strategy
     splitPyramidally: {
       // The interest APY fomula is (1 + rate) ^ 365 - 1
-      MIN_TO_LEND: 50, // The minimum amount to lend
+      MIN_TO_LEND: 150, // The minimum amount to lend
       UP_BOUND_RATE: 0.001, // The maximum interest (per day), 0.001 = 43.8% APY
       LOW_BOUND_RATE: 0.0001, // The minimum interest (per day), 0.0001 = 3.7% APY
       AMOUNT_GROW_EXP: 1.4, // The growth rate of the amount
