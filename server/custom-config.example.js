d258fda89e5570235036d6f28da63bdd8c182277
LineageH <lineage.hui@gmail.com>
2026-06-18 08:59:42 +0800

    remark fix

--- server/custom-config.example.js
+++ server/custom-config.example.js
@@ -7,7 +7,7 @@ module.exports = {
     // The default strategy is splitByRate, you need to edit the code in server/submit-funding-offer.js to change the strategy
     splitByRate: {
       MIN_TO_LEND: 150, // The minimum amount to lend (Bitfinex API minimum offer size is 150 USD)
-      MIN_APY: 0.09, // 0.7 = 7%, if the minimum APY is not met, the bot will not lend
+      MIN_APY: 0.09, // 0.09 = 9%, if the minimum APY is not met, the bot will not lend
       FRR_FACTOR: 0.95, // Offers rate = FRR * FRR_FACTOR * TIER_RATE_MULTIPLIERS
       TIER_RATE_MULTIPLIERS: [1.0, 1.04, 1.09, 1.14, 1.19, 1.25, 1.35, 1.45], // The multiplier for each tier to determine the offer rate
       TIER_WEIGHTS: [1.0, 1.5, 2.0, 2.5, 2.5, 2.0, 1.5, 1.0], // The weight for each tier
